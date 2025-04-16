00cab7841c4d52f1d5cb61db3fab5c79d6ece3ca
mateusfrade <mateus.frade@hotmail.com>
2025-04-16 22:38:55 +0000

    acrencentando a peça do cavalo no jogando_xadrez

--- jogando_xadrez.c
+++ jogando_xadrez.c
@@ -2,23 +2,22 @@
 
 int main(){
 
-int t = 0;
-int b = 0;
-int r = 0;
+int i = 0, j = 0, k = 0, l = 0; 
+int movimentoDoCavalo = 1;
 
         printf("Movimento da Torre\n");
         printf("5 casas para a direita\n");
-    for (int t = 0; t < 5; t++)
+    for (int i = 0; i < 5; i++)
     {
         printf("Direita\n");
     }
 
         printf("\nMovimento do Bispo\n");
         printf("5 casas na diagonal para cima e a direita\n");
-    while (b <= 5)
+    while (j <= 5)
     {
         printf("Cima, Direita\n");
-       b++;
+       j++;
     }
 
         printf("\nMovimento da Rainha\n");
@@ -26,10 +25,19 @@ int r = 0;
     do
     {
         printf("Esquerda\n");
-        r++;
-    } while (r < 8);
+        k++;
+    } while (k < 8);
     
-
+    printf("\nMovimento do Cavalo\n");
+    printf("2 casas para cima e 1 para a direita\n");
+while (movimentoDoCavalo --)
+{
+    for (l = 1; l <= 2; l++)
+    {
+        printf("Cima\n");
+    }
+    printf("Direita\n");
+}
 
  
     return 0;
